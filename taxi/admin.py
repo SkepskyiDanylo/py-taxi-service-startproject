c200bc19158093e6f4d0fe46658c855ab5317fe4
Danylo Skepskyi <kol230305@gmail.com>
2024-11-24 18:18:31 +0100

    add to add_fieldsets license_number

--- taxi/admin.py
+++ taxi/admin.py
@@ -11,6 +11,7 @@ admin.site.register(Manufacturer)
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number", )
     fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}), )
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number",)}), )
 
 
 @admin.register(Car)
